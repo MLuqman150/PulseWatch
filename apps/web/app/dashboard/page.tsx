795b8b21e0e166e287f7b885c81f2893f243b7b0
mluqman150 <muhammadluqman12080@outlook.com>
2026-07-30 01:24:12 +0500

    Styles changes in the dashboard page

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -121,52 +121,63 @@ export default function DashboardPage() {
     return (
         <>
             <ProtectedRoutes>
-                <h1 className="text-center">Dashboard</h1>
-                <button onClick={handleLogout} className="cursor-pointer">Logout</button>
-                <form action="" method="POST" onSubmit={handleSubmit(onSubmit)}>
-                    <div>
-                        <label htmlFor="url">Add Website</label>
-                        <input
-                            {...register("url", { required: "Website url is required" })}
-                            type="text"
-                            id="url"
-                            name="url"
-                            placeholder="Add a website"
-                        />
-                        {errors?.url && (
-                            <p className="text-red-600 text-sm">
-                                {errors?.url?.message}
-                            </p>
-                        )}
-                        <button type="submit" disabled={!isValid || isSubmitting}>Add</button>
-                    </div>
-                </form>
+                <div className="flex items-center justify-center">
+                    <h1 className="text-center text-xl font-semibold">Dashboard</h1>
+                    <button onClick={handleLogout} className=" my-2 bg-black p-2 text-white font-medium rounded-md cursor-pointer transition-colors duration-200 hover:bg-gray-800">Logout</button>
+                </div>
+                <div className="min-h-screen flex flex-col items-center justify-center">
+                    <form action="" method="POST" onSubmit={handleSubmit(onSubmit)}>
+                        <div>
+                            <label htmlFor="url">Add Website</label>
+                            <input
+                                {...register("url", { required: "Website url is required" })}
+                                type="text"
+                                id="url"
+                                name="url"
+                                placeholder="Add a website"
+                                className="w-full max-w-md h-10 border rounded-lg px-4"
+                            />
+                            {errors?.url && (
+                                <p className="text-red-600 text-sm">
+                                    {errors?.url?.message}
+                                </p>
+                            )}
+                            <button 
+                                type="submit" 
+                                disabled={!isValid || isSubmitting}
+                                className="my-2 bg-black p-2 text-white font-medium rounded-md cursor-pointer transition-colors duration-200 hover:bg-gray-800"
+                            >
+                                Add
+                            </button>
+                        </div>
+                    </form>
 
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Website</th>
-                                <th>Created At</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {websites.map((website, index) => (
-                                <tr key={website.id}>
-                                    <td>{index + 1}</td>
-                                    <td>
-                                        <Link href={`/dashboard/${website.id}`} className="hover:underline hover:text-red-500">
-                                            {website.url}
-                                        </Link>
-                                    </td>
-                                    <td>{new Date(website.createdAt).toLocaleDateString()}</td>
-                                    <td><button className="text-red-400 cursor-pointer hover:underline" onClick={()=> {handleDelete(website.id)}}>Delete</button></td>
+                    <div className="flex"> 
+                        <table className="">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Website</th>
+                                    <th>Created At</th>
+                                    <th>Actions</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {websites.map((website, index) => (
+                                    <tr key={website.id}>
+                                        <td>{index + 1}</td>
+                                        <td>
+                                            <Link href={`/dashboard/${website.id}`} className="hover:underline hover:text-red-500">
+                                                {website.url}
+                                            </Link>
+                                        </td>
+                                        <td>{new Date(website.createdAt).toLocaleDateString()}</td>
+                                        <td><button className="text-red-400 cursor-pointer hover:underline" onClick={()=> {handleDelete(website.id)}}>Delete</button></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </ProtectedRoutes>
         </>
